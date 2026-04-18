57812e54aabf0dcd82bce87a938da6a318209283
Pomaris, John Nathan (Student) <pomaris.402145@balagtas.sti.edu.ph>
2026-04-18 09:30:41 +0800

    Add project files.

---
+++ StudentInfoApplication/frmStudentInfo.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace StudentInfoApplication
+{
+    partial class frmStudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblpnHeader = new System.Windows.Forms.TableLayoutPanel();
+            this.pnHeader = new System.Windows.Forms.Panel();
+            this.tblpnMain = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tblpnHeader.SuspendLayout();
+            this.pnHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblpnHeader
+            // 
+            this.tblpnHeader.BackColor = System.Drawing.Color.PapayaWhip;
+            this.tblpnHeader.ColumnCount = 1;
+            this.tblpnHeader.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnHeader.Controls.Add(this.pnHeader, 0, 0);
+            this.tblpnHeader.Controls.Add(this.tblpnMain, 0, 1);
+            this.tblpnHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblpnHeader.Location = new System.Drawing.Point(0, 0);
+            this.tblpnHeader.Margin = new System.Windows.Forms.Padding(0);
+            this.tblpnHeader.Name = "tblpnHeader";
+            this.tblpnHeader.RowCount = 2;
+            this.tblpnHeader.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tblpnHeader.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88F));
+            this.tblpnHeader.Size = new System.Drawing.Size(800, 450);
+            this.tblpnHeader.TabIndex = 0;
+            // 
+            // pnHeader
+            // 
+            this.pnHeader.BackColor = System.Drawing.Color.Red;
+            this.pnHeader.Controls.Add(this.label1);
+            this.pnHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnHeader.Margin = new System.Windows.Forms.Padding(0);
+            this.pnHeader.Name = "pnHeader";
+            this.pnHeader.Size = new System.Drawing.Size(800, 54);
+            this.pnHeader.TabIndex = 0;
+            // 
+            // tblpnMain
+            // 
+            this.tblpnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tblpnMain.ColumnCount = 3;
+            this.tblpnMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblpnMain.Location = new System.Drawing.Point(0, 54);
+            this.tblpnMain.Margin = new System.Windows.Forms.Padding(0);
+            this.tblpnMain.Name = "tblpnMain";
+            this.tblpnMain.RowCount = 3;
+            this.tblpnMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblpnMain.Size = new System.Drawing.Size(800, 396);
+            this.tblpnMain.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(89, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // frmStudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tblpnHeader);
+            this.Name = "frmStudentInfo";
+            this.Text = "Form1";
+            this.tblpnHeader.ResumeLayout(false);
+            this.pnHeader.ResumeLayout(false);
+            this.pnHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblpnHeader;
+        private System.Windows.Forms.Panel pnHeader;
+        private System.Windows.Forms.TableLayoutPanel tblpnMain;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
